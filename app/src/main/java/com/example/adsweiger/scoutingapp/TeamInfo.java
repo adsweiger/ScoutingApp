e2d1ced1e3c207aa489f334d4ff96a37cd43725d
Adam Sweiger <adsweiger@student.linnmar.org>
2018-04-26 15:29:44 -0500

    Setup

--- app/src/main/java/com/example/adsweiger/scoutingapp/TeamInfo.java
+++ app/src/main/java/com/example/adsweiger/scoutingapp/TeamInfo.java
@@ -1,18 +1,27 @@
 package com.example.adsweiger.scoutingapp;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.drm.DrmStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class TeamInfo extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("comment", "Point C Reached");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_info);
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        }
     }
 
     public void onBackClick(View view) {
