a321dfc1925b78e9c20b6b40bf3b9b5c4f0822a1
Eamonn Ryan <earyan@student.linnmar.org>
2018-05-15 15:29:46 -0500

    TeamMatches stuff

--- app/src/main/java/com/example/adsweiger/scoutingapp/AddMatches.java
+++ app/src/main/java/com/example/adsweiger/scoutingapp/AddMatches.java
@@ -3,14 +3,26 @@ package com.example.adsweiger.scoutingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AddMatches extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_matches);
+        if (b == null) {
+            Log.i("stuff", "Null bundle");
+            return;
+        }
+        final TextView redOne = findViewById(R.id.RedTeam1);
+        String getRedOne = b.getString("red one");
+        redOne.setText(getRedOne);
     }
     public void onDoneClick(View view) {
         Intent done = new Intent(this, Matches.class);
